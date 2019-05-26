6b5f71a12c4fa55945c496d642167def47f7829b
guangpengju <guangpengju@163.com>
2019-05-26 13:17:02 +0800

    springcloud项目修改

--- springcloud-project-demo/springcloud-client-feign/src/main/java/com/gpj/springcloud/client/api/ServerApi.java
+++ springcloud-project-demo/springcloud-client-feign/src/main/java/com/gpj/springcloud/client/api/ServerApi.java
@@ -3,7 +3,7 @@ package com.gpj.springcloud.client.api;
 import com.gpj.springcloud.server.Server;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(value = "PROJECT-SERVER",fallback = ServerApiFallback.class,path = "server")
+@FeignClient(value = "PROJECT-SERVER", fallback = ServerApiFallback.class, path = "server")
 public interface ServerApi extends Server {
     
 }
